e80e90baf937be91cf31a0f75b30a67fe8117cf2
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-03-29 14:30:12 +0300

    Удалены лишнее файлы + исправлены

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module org.fxstudy.oraclefx {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
-
-
     opens org.fxstudy.oraclefx to javafx.fxml;
     exports org.fxstudy.oraclefx;
 }
